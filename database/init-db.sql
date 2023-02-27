781ecbad998f4c5d4e58ff15d985c4c9745d38db
PierreBeur <cbeurtheret@ucsd.edu>
2023-02-27 07:56:25 -0800

    Update init-db.sql

--- database/init-db.sql
+++ database/init-db.sql
@@ -7,8 +7,16 @@ drop table if exists users;
 
 -- 1. Create the users table
 create table if not exists users (
-  -- [INSERT CODE HERE]
+  id         integer auto_increment primary key,
+  first_name varchar(30) not null,
+  last_name  varchar(30) not null,
+  created_at timestamp
 );
 
 -- 2. Insert initial seed records into the table
--- [INSERT CODE HERE]
+insert into users (first_name, last_name, created_at) values
+  ("Donald",  "Chamberlin", current_timestamp),
+  ("Raymond", "Boyce",      current_timestamp),
+  ("Brian",   "Kernighan",  current_timestamp),
+  ("Dennis",  "Ritchie",    current_timestamp)
+;
